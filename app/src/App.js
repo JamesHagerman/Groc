7f209befedfdb0d31cef1a309875facb94c86a9e
James Hagerman <james.hagerman@gmail.com>
2018-04-09 00:44:11 -0700

    Initial commit.

---
+++ app/src/App.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import '@blueprintjs/core/dist/blueprint.css'
+import '@blueprintjs/core/dist/blueprint-icons.css'
+
+import { Button, Intent, Spinner, DatePickerFactory } from '@blueprintjs/core';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App" className='pt-dark'>
+        <div>
+          <button type='button' className='pt-button' tabIndex='0'>Some Button</button>
+          <Spinner intent={Intent.PRIMARY} />
+          <Button intent={Intent.SUCCESS}>
+            deeeeeerp
+          </Button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
