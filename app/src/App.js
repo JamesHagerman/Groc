6e199a266e31a0fb5c6ed787f3a44019e4f59d7d
James Hagerman <james.hagerman@gmail.com>
2018-04-15 16:04:03 -0700

    Getting the item list working again. Adding a default set of items to help populate the list. Adding the ability to hide unchecked items. Adding google analytics tracking code. Adding state debug output just to make some things easier.

--- app/src/App.js
+++ app/src/App.js
@@ -8,12 +8,15 @@ import {SortableContainer, SortableElement, SortableHandle, arrayMove} from 'rea
 import croc from './croc.png'
 import './App.css';
 
+import defaultItems from './defaultItems'
+
 const initialState = {
   itemList: [],
   toAdd: '',
   sotableDisabled: true,
   helpIsOpen: true,
-  controlsAreOpen: false
+  controlsAreOpen: false,
+  checkedHidden: false
 }
 
 class App extends Component {
@@ -21,6 +24,7 @@ class App extends Component {
   constructor(props) {
     super(props)
 
+    // Local storage/state management
     this.saveState = this.saveState.bind(this)
     this.resetState = this.resetState.bind(this)
     this.recoverState = this.recoverState.bind(this)
@@ -32,19 +36,54 @@ class App extends Component {
       this.state = recoveredState
     }
 
-    this.handleEnabledChange = this.handleEnabledChange.bind(this)
-    this.handleItemEdit = this.handleItemEdit.bind(this)
-
-    this.buildNewItem = this.buildNewItem.bind(this)
-    this.handleNewItem = this.handleNewItem.bind(this)
-    this.handleNewItemKeyPress = this.handleNewItemKeyPress.bind(this)
-
-    this.handleSortEnd = this.handleSortEnd.bind(this)
+    // Useful tools
     this.handleShowHelp = this.handleShowHelp.bind(this)
     this.handleShowControls = this.handleShowControls.bind(this)
+    this.handleShowChecked = this.handleShowChecked.bind(this)
+    this.handleAddCommonItems = this.handleAddCommonItems.bind(this)
+
+    // Item stuff
+    this.buildNewItem = this.buildNewItem.bind(this)
+    this.handleItemEdit = this.handleItemEdit.bind(this)
+    this.handleNewItem = this.handleNewItem.bind(this)
+    this.handleNewItemKeyPress = this.handleNewItemKeyPress.bind(this)
     this.handleCheck = this.handleCheck.bind(this)
 
+    // sortable list stuff
+    this.handleEnabledChange = this.handleEnabledChange.bind(this)
+    this.handleSortEnd = this.handleSortEnd.bind(this)
     this.renderSortableComponent = this.renderSortableComponent.bind(this)
+  }
+
+  saveState() {
+    console.log('Saving Groc State:', this.state)
+    localStorage.setItem('grocStateData', JSON.stringify(this.state));
+  }
+
+  resetState() {
+    console.log('Resetting Groc State...')
+    // TODO: Find a better way to set state on mount and any other time:
+    this.state = initialState
+    this.setState(initialState, this.saveState)
+  }
+
+  recoverState() {
+    const grocStateDataJSON = localStorage.getItem('grocStateData');
+    let grocStateData = {}
+    if (grocStateDataJSON === null || grocStateDataJSON === '"[object Object]"') {
+      console.log('No Groc State to recover...')
+      return null
+    }
+    try {
+      grocStateData = JSON.parse(grocStateDataJSON)
+    }
+    catch(error) {
+      console.error('Groc State was invalid! Resetting it...');
+      prompt('Groc state was invalid. Here it is if you want to debug it:', JSON.stringify(this.state))
+      return null
+    }
+    console.log('Got Groc State Data from localStorage:', JSON.stringify(grocStateData,0,2))
+    return grocStateData
   }
 
   handleShowHelp() {
@@ -61,23 +100,31 @@ class App extends Component {
     }, this.saveState)
   }
 
-  handleEnabledChange(event) {
-    const target = event.target;
-    const value = target.type === 'checkbox' ? target.checked : target.value;
-
-    console.log(this.state.sotableDisabled)
+  handleShowChecked() {
     this.setState({
       ...this.state,
-      sotableDisabled: value
+      checkedHidden: !this.state.checkedHidden
     }, this.saveState)
   }
 
-  handleItemEdit(event) {
-    // console.log(event.target.value)
+  handleAddCommonItems() {
+    let newItemList = this.state.itemList.concat()
+
+    defaultItems.forEach((itemName) => {
+      let itemAlreadyInList = newItemList.find((item) => {
+        return item.itemName === itemName
+      })
+      if (typeof itemAlreadyInList === 'undefined') {
+        let newItem = this.buildNewItem(itemName)
+        // console.log('Adding', newItem)
+        newItemList.push(newItem)
+      }
+    })
+
     this.setState({
       ...this.state,
-      toAdd: event.target.value
-    }, this.saveState)
+      itemList: newItemList
+    })
   }
 
   buildNewItem(itemName) {
@@ -86,6 +133,14 @@ class App extends Component {
       itemName,
       itemChecked: false
     }
+  }
+
+  handleItemEdit(event) {
+    // console.log(event.target.value)
+    this.setState({
+      ...this.state,
+      toAdd: event.target.value
+    }, this.saveState)
   }
 
   handleNewItem() {
@@ -110,6 +165,45 @@ class App extends Component {
     }
   }
 
+  handleCheck(event) {
+    const target = event.target
+    const checked = target.checked
+    const uuid = target.value
+    console.log('OK!', uuid, checked)
+
+    const updatedItemList = this.state.itemList.concat()
+    console.log('currentList', updatedItemList)
+    const modifiedItemId = updatedItemList.findIndex((item, index) => {
+      console.warn('item', item)
+      return item.itemUuid === uuid;
+    })
+
+    console.log('modifiedItemId', modifiedItemId)
+    
+    let itemToToggle = updatedItemList[modifiedItemId]
+    itemToToggle.itemChecked = !itemToToggle.itemChecked
+
+    console.log('omfg:', JSON.stringify(itemToToggle, 0, 2))
+    updatedItemList.splice(modifiedItemId, 1, itemToToggle)
+
+    console.log('updatedItemList', updatedItemList)
+    this.setState({
+      ...this.state,
+      itemList: updatedItemList
+    }, this.saveState)
+  }
+
+  handleEnabledChange(event) {
+    const target = event.target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+
+    console.log(this.state.sotableDisabled)
+    this.setState({
+      ...this.state,
+      sotableDisabled: value
+    }, this.saveState)
+  }
+
   handleSortEnd = ({oldIndex, newIndex}) => {
     this.setState({
       ...this.state,
@@ -117,76 +211,24 @@ class App extends Component {
     }, this.saveState)
   };
 
-  handleCheck(event) {
-    const target = event.target
-    const checked = target.checked
-    const uuid = target.value
-    console.log('OK!', uuid, checked)
-
-    const currentList = this.state.itemList.concat()
-    // console.log('currentList', currentList)
-    const modifiedItemId = currentList.findIndex((item, index) => {
-      // console.warn('item', item)
-      return item.itemUuid === uuid;
-    })
-
-    // console.log('modifiedItemId', modifiedItemId)
-    
-    let itemToToggle = currentList[modifiedItemId]
-    itemToToggle.itemChecked = !itemToToggle.itemChecked
-
-    // console.log('omfg:', JSON.stringify(itemToToggle, 0, 2))
-    let updatedList = currentList.splice(modifiedItemId, 1, itemToToggle)
-
-    // console.log('updatedList', updatedList)
-    this.setState({
-      ...this.state,
-      itemList: updatedList
-    }, this.saveState)
-  }
-
-  saveState() {
-    console.log('Saving Groc State:', this.state)
-    localStorage.setItem('grocStateData', JSON.stringify(this.state));
-  }
-
-  resetState() {
-    console.log('Resetting Groc State...')
-    prompt('please press ctrl+c to copy the text below', JSON.stringify(this.state))
-    // TODO: Find a better way to set state on mount and any other time:
-    this.state = initialState
-    this.setState(initialState, this.saveState)
-  }
-
-  recoverState() {
-    const grocStateDataJSON = localStorage.getItem('grocStateData');
-    let grocStateData = {}
-    if (grocStateDataJSON === null || grocStateDataJSON === '"[object Object]"') {
-      console.log('No Groc State to recover...')
-      return null
-    }
-    try {
-      grocStateData = JSON.parse(grocStateDataJSON)
-    }
-    catch(error) {
-      console.error('Groc State was invalid! Resetting it...');
-      return null
-    }
-    console.log('Got Groc State Data from localStorage:', JSON.stringify(grocStateData,0,2))
-    return grocStateData
-  }
-
   renderSortableComponent() {
     const items = this.state.itemList
     return (
-      <SortableList items={items} onSortEnd={this.handleSortEnd} disabled={this.state.sotableDisabled} handleCheck={this.handleCheck}/>
-    )// lockAxis='y'
+      <SortableList
+        items={items}
+        onSortEnd={this.handleSortEnd}
+        disabled={this.state.sotableDisabled}
+        handleCheck={this.handleCheck}
+        checkedHidden={this.state.checkedHidden}/>
+    )
   }
+  
   render() {
     // TODO: Upgrade blueprint to something AFTER 2.1.1 because checkboxes were broken in that version
     return (
       <div className='App pt-app pt-dark'>
         <div className='groc-outter'>
+
           <div className={`groc-doc column`} >
             <Collapse isOpen={this.state.helpIsOpen} className='docs'>
               <img src={croc} className={this.state.helpIsOpen ? 'large' : 'small'} />
@@ -206,14 +248,18 @@ class App extends Component {
             <Button onClick={this.handleShowHelp} className='showHide'>
                 {this.state.helpIsOpen ? "Hide" : "Show"} Help
             </Button>
-            <Button onClick={this.handleShowControls} className='showHide'>
-                {this.state.controlsAreOpen ? "Hide" : "Show"} Tools
+            <Button onClick={this.handleAddCommonItems}>
+              Add Common Items
+            </Button>
+            <Button onClick={this.handleShowChecked} className='showChecked'>
+                {this.state.checkedHidden ? "Show" : "Hide"} Unchecked Items
             </Button>
           </div>
+
           <div className='groc-list'>
-            {/* Items you need: */}
             {this.renderSortableComponent()}
           </div>
+
           <div className='groc-control'>
             <input
               className="pt-input .modifier"
@@ -228,7 +274,7 @@ class App extends Component {
             </Button>
             <div>
               <label className="pt-label .modifier">
-                Disable Item Sorting:
+                Disable Sorting: 
                 <input
                   name="isDraggable"
                   type="checkbox"
@@ -236,20 +282,17 @@ class App extends Component {
                   onChange={this.handleEnabledChange} />
                 {/* <Checkbox label="Toggle Resortable" onChange={this.handleEnabledChange}/> */}
               </label>
+              <Button onClick={this.handleShowControls} className='showHide'>
+                {this.state.controlsAreOpen ? "Hide" : "Show"} Tools
+              </Button>
             </div>
             <Collapse isOpen={this.state.controlsAreOpen} className='controls'>
               <Button onClick={this.resetState}>
                   Reset Groc
               </Button>
-              <div className='debug'>
-                Debug:
-                {JSON.stringify(this.state)}
-              </div>
             </Collapse>
-            <div>
-              
-            </div>
           </div>
+
           <div className='groc-footer'>
             <p>
               Thanks for trying Groc! <img src={croc} className={this.state.helpIsOpen ? 'tiny' : 'small'} /><br/> 
@@ -258,7 +301,15 @@ class App extends Component {
             <p>
               © 2018 <a href='https://twitter.com/jamisnemo'>James Hagerman</a>
             </p>
+            <Collapse isOpen={this.state.controlsAreOpen} className='debug'>
+              <div className='debug'>
+                Debug: <br />
+                {JSON.stringify(this.state)}
+              </div>
+            </Collapse>
+            
           </div>
+
         </div>
       </div>
     );
@@ -272,9 +323,15 @@ const SortableItem = SortableElement((props) => {
       itemName,
       itemChecked
     },
-    handleCheck
+    handleCheck,
+    checkedHidden
   } = props
   // console.warn('props', props)
+
+  if (checkedHidden && !itemChecked) {
+    return null
+  }
+
   return (
     <li>
       <label className="pt-label .modifier">
@@ -292,11 +349,23 @@ const SortableItem = SortableElement((props) => {
 });
 
 const SortableList = SortableContainer((props) => {
-  const {items, disabled, itemChecked, handleCheck} = props
+  const {
+    items,
+    disabled,
+    itemChecked,
+    handleCheck,
+    checkedHidden
+  } = props
   return (
     <ul>
       {items.map((item, index) => (
-        <SortableItem key={`item-${index}`} index={index} item={item} disabled={disabled} handleCheck={handleCheck}/>
+        <SortableItem
+          key={`item-${index}`}
+          index={index}
+          item={item}
+          disabled={disabled}
+          handleCheck={handleCheck}
+          checkedHidden={checkedHidden}/>
       ))}
     </ul>
   );
